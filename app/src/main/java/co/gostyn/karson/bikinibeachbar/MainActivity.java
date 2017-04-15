8b8dbb4f14f58258bfd659867a8e22fb024ac42a
karsonpl <karson@gostyn.co>
2017-04-15 22:47:21 +0200

    2017.04.15

--- app/src/main/java/co/gostyn/karson/bikinibeachbar/MainActivity.java
+++ app/src/main/java/co/gostyn/karson/bikinibeachbar/MainActivity.java
@@ -3,17 +3,79 @@ package co.gostyn.karson.bikinibeachbar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import javax.security.auth.login.LoginException;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+
+import static android.R.transition.move;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Oferta.class);
+
+
+        /*Intent intent = new Intent(this, Oferta.class);
         startActivity(intent);
-        finish();
+        finish();*/
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView btn = (ImageView) findViewById(R.id.imageView);
+
+
+
+
+                moveViewToScreenCenter(btn);
+            }
+        });
 
     }
+
+    private void moveViewToScreenCenter( View view )
+    {
+        RelativeLayout root = (RelativeLayout) findViewById( R.id.rl );
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics( dm );
+
+
+
+
+    /*    int statusBarOffset = dm.heightPixels - root.getMeasuredHeight();
+
+        int originalPos[] = new int[2];
+        view.getLocationOnScreen( originalPos );
+
+        int xDest = dm.widthPixels/2;
+        xDest -= (view.getMeasuredWidth()/2);
+        int yDest = dm.heightPixels/2 - (view.getMeasuredHeight()/2) - statusBarOffset;*/
+
+
+
+        //TranslateAnimation anim = new TranslateAnimation( 0, xDest - originalPos[0] , 0, yDest - originalPos[1] );
+        TranslateAnimation anim = new TranslateAnimation(0,dm.widthPixels/2,0,-dm.heightPixels/2);
+        anim.setDuration(500);
+        anim.setFillAfter( true );
+        view.startAnimation(anim);
+    }
+
+
 }
