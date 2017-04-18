243f68baff520686411690ce9c695cf5f3eba577
karsonPL <karson@gostyn.co>
2017-04-18 14:52:00 +0200

    2017.04.18

--- app/src/main/java/co/gostyn/karson/bikinibeachbar/MainActivity.java
+++ app/src/main/java/co/gostyn/karson/bikinibeachbar/MainActivity.java
@@ -1,63 +1,60 @@
 package co.gostyn.karson.bikinibeachbar;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import javax.security.auth.login.LoginException;
-import javax.xml.transform.sax.SAXTransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-
-import static android.R.transition.move;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    public Button button;
+
+
+    @Bind(R.id.btnMenu)
+    Button btnMenu;
+    @Bind(R.id.btnWydarzenia)
+    Button btnWydarzenia;
+    @Bind(R.id.btnInformacje)
+    Button btnInformacje;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
 
 
         /*Intent intent = new Intent(this, Oferta.class);
         startActivity(intent);
         finish();*/
 
-        button = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        moveViewToScreenCenter(btnMenu,800);
+        moveViewToScreenCenter(btnWydarzenia,1000);
+        moveViewToScreenCenter(btnInformacje,1200);
+
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageView btn = (ImageView) findViewById(R.id.imageView);
 
-
-
-
-                moveViewToScreenCenter(btn);
+                moveViewToScreenCenter(btnMenu,800);
+                moveViewToScreenCenter(btnWydarzenia,1000);
+                moveViewToScreenCenter(btnInformacje,1200);
             }
         });
 
     }
 
-    private void moveViewToScreenCenter( View view )
-    {
-        RelativeLayout root = (RelativeLayout) findViewById( R.id.rl );
+    private void moveViewToScreenCenter(View view, int duration) {
+
+        RelativeLayout root = (RelativeLayout) findViewById(R.id.rl);
         DisplayMetrics dm = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics( dm );
-
-
-
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
 
     /*    int statusBarOffset = dm.heightPixels - root.getMeasuredHeight();
 
@@ -68,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
         xDest -= (view.getMeasuredWidth()/2);
         int yDest = dm.heightPixels/2 - (view.getMeasuredHeight()/2) - statusBarOffset;*/
 
-
-
         //TranslateAnimation anim = new TranslateAnimation( 0, xDest - originalPos[0] , 0, yDest - originalPos[1] );
-        TranslateAnimation anim = new TranslateAnimation(0,dm.widthPixels/2,0,-dm.heightPixels/2);
-        anim.setDuration(500);
-        anim.setFillAfter( true );
+        TranslateAnimation anim = new TranslateAnimation(-1000, 0, 0, 0);
+        anim.setDuration(duration);
+        anim.setFillAfter(true);
         view.startAnimation(anim);
     }
+
+
 
 
 }
