17f87aff1c5a2b22452f8d0b2d14cb5dda25d00c
karsonpl <karson@gostyn.co>
2017-04-29 20:31:28 +0200

    2017.04.29

--- app/src/main/java/co/gostyn/karson/bikinibeachbar/DrinkiActivity.java
+++ app/src/main/java/co/gostyn/karson/bikinibeachbar/DrinkiActivity.java
@@ -1,23 +1,28 @@
 package co.gostyn.karson.bikinibeachbar;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class DrinkiActivity extends AppCompatActivity {
 
@@ -98,6 +103,10 @@ public class DrinkiActivity extends AppCompatActivity {
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        @Bind(R.id.drinkGfx)
+        ImageView drinkGfx;
+        @Bind(R.id.drinkNazwa)
+        TextView drinkNazwa;
 
         public PlaceholderFragment() {
         }
@@ -118,9 +127,21 @@ public class DrinkiActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_drinki, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+         //   TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+         //   textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            ButterKnife.bind(this, rootView);
+
+
+
+
             return rootView;
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            ButterKnife.unbind(this);
         }
     }
 
--- app/src/main/java/co/gostyn/karson/bikinibeachbar/WydarzeniaActivity.java
+++ app/src/main/java/co/gostyn/karson/bikinibeachbar/WydarzeniaActivity.java
@@ -11,5 +11,6 @@ public class WydarzeniaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wydarzenia);
 
 
+
     }
 }
